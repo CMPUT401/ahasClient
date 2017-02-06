d2d5fd8f638abf7cf000e027f8e5a1e0a7ffd083
Kristy <knewbury@ualberta.ca>
2017-02-06 12:32:12 -0800

    setting up webrepo, already started login page

---
+++ app/adapters/application.js
@@ -0,0 +1,9 @@
+import ActiveModelAdapter from 'active-model-adapter';  
+import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';  
+import config from '../config/environment';
+
+
+export default ActiveModelAdapter.extend(DataAdapterMixin, {  
+	host: `${config.host}`,
+	    authorizer: 'authorizer:custom'
+	    });
---
+++ app/controllers/login.js
@@ -0,0 +1,17 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({  
+  session: Ember.inject.service(),
+
+  actions: {
+    authenticate: function() {
+      var credentials = this.getProperties
+        ('identification', 'password'),
+        authenticator = 'authenticator:jwt';
+      this.get('session').authenticate(authenticator, 
+        credentials).catch((reason)=>{
+        this.set('errorMessage', reason.error || reason);
+      });
+    }
+  }
+});
