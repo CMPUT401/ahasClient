13b9226a6de6302b0110a0171c61b53ad2169f47
Kristy <knewbury@ualberta.ca>
2017-02-14 14:55:06 -0800

    user object is now serialized lowercase

--- app/models/user.js
+++ app/models/user.js
@@ -2,7 +2,9 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
 
-    email: DS.attr(),
-    password: DS.attr()
+    email: DS.attr('string', {
+        lowercase: true
+    }),
+    password: DS.attr('string')
 
 });
