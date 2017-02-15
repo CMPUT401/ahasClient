d57eff5297280c17f26c14643e17b223f62cf630
MarcOlivierLajeunesse <malajeun@ualberta.ca>
2017-02-15 15:25:57 -0700

    began implementing AJAX into the route and controller

--- app/controllers/new-client.js
+++ app/controllers/new-client.js
@@ -1,11 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+	ajax: Ember.inject.service(),
 	actions: {
-		submitNewCLient(){
-			// TODO
-			// check input
-			// send via ajax
+		sendRequest(){
+			return this.get('ajax').request('/api/client' ,{
+				method: 'POST',
+				client: {
+					name:  ,
+					address:  ,
+					phoneNumber:  ,
+					email:  ,
+					licos:  ,
+					socialAssistance:  ,
+					pets:  
+				}
+			});
 		}
 	}
 });
--- app/routes/new-client.js
+++ app/routes/new-client.js
@@ -1,14 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	ajax: Ember.inject.service(),
 	model() {
-		return this.store.createRecord('newClient');
+		//return this.store.createRecord('newClient');
+		return this.get('ajax').request('/posts');
 	},
 	actions:{
 		createNewClient(){
 			let clientInfo = this.modelFor(this.routeName);
 			clientInfo.save().then(function () {
-
+				//sendRequest();
 				//this.transitionTo('new-client');
 			}).catch(function (reason){
 
