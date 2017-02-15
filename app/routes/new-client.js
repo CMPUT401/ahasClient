de4528848920326db4a438476ca614109664add2
MarcOlivierLajeunesse <malajeun@ualberta.ca>
2017-02-15 14:15:29 -0700

    added model. route now creates a model object

--- app/routes/new-client.js
+++ app/routes/new-client.js
@@ -2,11 +2,17 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 	model() {
-		
+		return this.store.createRecord('newClient');
 	},
 	actions:{
 		createNewClient(){
-			
+			let clientInfo = this.modelFor(this.routeName);
+			newClient.save().then(function () {
+
+				//this.transitionTo('new-client');
+			}).catch(function (reason){
+
+			});
 		}
 	}
 });
