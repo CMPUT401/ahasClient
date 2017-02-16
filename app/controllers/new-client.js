ecb55ba07983d63796f443dc31978a8940804e4a
MarcOlivierLajeunesse <malajeun@ualberta.ca>
2017-02-16 11:39:57 -0700

    can send POST request and began implementing http status handlers

--- app/controllers/new-client.js
+++ app/controllers/new-client.js
@@ -4,7 +4,8 @@ export default Ember.Controller.extend({
 	ajax: Ember.inject.service(),
 	actions: {
 		sendRequest(){
-			return this.get('ajax').post('/api/client' , {
+			self = this;
+			let ajaxPost = this.get('ajax').post('/api/client' , {
 				type: 'application/json',
 				data: {client: {
 					// name: model.clientName,
@@ -13,15 +14,24 @@ export default Ember.Controller.extend({
 					// email: model.clientEmail,
 					// licos: model.clientLICO,
 					// socialAssistance: model.clientAS
-					name: 'bob',
+					name: '',
 					address: '123 somewehere st, Edmonton',
 					phoneNumber: '780-555-1234',
-					email: 'some@email.com',
+					email: 'someBoby@email.com',
 					licos: '12345',
 					socialAssistance: '4313',
 					pets: ''
-				}}
-			});
+				}}, 
+			}).then(function(data){
+					console.log("status is " + JSON.stringify(data));
+					self.transitionToRoute('login');
+				},
+				function(data){
+					console.log("status is " + JSON.stringify(data));
+				});
+			//createNewCLient();
+			//this.transitionToRoute('/login');
+			return ajaxPost;
 		}
 	}
 });
--- app/routes/new-client.js
+++ app/routes/new-client.js
@@ -4,44 +4,19 @@ import {isAjaxError, isNotFoundError, isForbiddenError} from 'ember-ajax/errors'
 export default Ember.Route.extend({
 	ajax: Ember.inject.service(),
 	model() {
-		//return this.store.createRecord('newClient');
-		//return this.get('ajax').request('/api/client');
-		
-		/*
-
-		///// Will cause 406
-		const ajax = this.get('ajax');
-
-		return ajax.request('/api/client').catch(function(error){
-			if (isNotFoundError(error)) {
-	          // handle 404 errors here 
-	          return;
-	        }
-	 
-	        if (isForbiddenError(error)) {
-	          // handle 403 errors here 
-	          return;
-	        }
-	 
-	        if(isAjaxError(error)) {
-	          // handle all other AjaxErrors here 
-	          return;
-	        }
-	 
-	        // other errors are handled elsewhere 
-	        throw error;
-		});
-*/
+		return this.store.createRecord('newClient');
 	}
 	// actions:{
 	// 	createNewClient(){
-	// 		let clientInfo = this.modelFor(this.routeName);
-	// 		clientInfo.save().then(function () {
-	// 			//sendRequest();
-	// 			//this.transitionTo('new-client');
-	// 		}).catch(function (reason){
+	// 		// let clientInfo = this.modelFor(this.routeName);
+	// 		// clientInfo.save().then(function () {
+	// 		// 	//sendRequest();
+	// 		// 	//this.transitionTo('new-client');
+	// 		// }).catch(function (reason){
 
-	// 		});
+	// 		// });
+	// 		console.log("made new client");
+	// 		this.transitionTo('new-client');
 	// 	}
 	// }
 });
