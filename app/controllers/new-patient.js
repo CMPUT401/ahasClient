730e038b73e97465a5b64301fb5230da10640438
erichsueh <erichsueh1337@hotmail.com>
2017-02-17 00:00:40 -0700

    Finished controller, gona pull master and test to see if it works

--- app/controllers/new-patient.js
+++ app/controllers/new-patient.js
@@ -8,9 +8,24 @@ export default Ember.Controller.extend({
 			let ajaxPost=this.get('ajax').post('/api/client',{
 				type: 'application/json',
 				data: {patient:{
-					name: this.get('patientName')
-				}}
-			})
+					client: 'id',
+					name: this.get('patientName'),
+					species: this.get('patientSpecies'),
+					age: this.get('patientAge'),
+					color: this.get('patientColor'),
+					tattoo: this.get('patientTatoo'),
+					microchip: this.get('patientMicrochip'),
+					gender:this.get('patientGender'),
+					status:this.get('patientStatus')
+				}},
+			}).then(function(data){
+				console.log("statis is " + JSON.stringify(data));
+				self.transitionToRoute('login');
+			},
+			function(data){
+				console.log("statis is " + JSON.stringify(data));
+			});
+		return ajaxPost;
 		}
 	}
-})
+});
