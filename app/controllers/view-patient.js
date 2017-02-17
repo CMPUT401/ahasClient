b13e1478e5936e26a1e2dc24e75de6870ab3d24f
erichsueh <erichsueh1337@hotmail.com>
2017-02-17 04:03:38 -0700

    Finished client docs

--- app/controllers/view-patient.js
+++ app/controllers/view-patient.js
@@ -3,5 +3,14 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
 	patientFirstName: 'Bob',
 	clientLastName: 'Bobbertson',
-	clientFirstName: 'Boob'
+	clientFirstName: 'Fred',
+	clientAddress: '22554 48th Ave NW Edmonton Alberta, Canada',
+	clientPhoneNumber: '666-666-6666',
+	clientEmail: '123dd@5d5dd.ca',
+
+	clientDocumentLICO: 'Yes',
+	clientDocumentAISH: 'Yes',
+	clientDocumentSA: 'Yes',
+	clientNotes: 'Smells bad?'
+
 })
