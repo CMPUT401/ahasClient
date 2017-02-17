d7b7b923e385dc926461174e67e1167cb2834d07
erichsueh <erichsueh1337@hotmail.com>
2017-02-17 04:37:53 -0700

    finished mock data, going to try getting data from patient only

--- app/controllers/view-patient.js
+++ app/controllers/view-patient.js
@@ -1,16 +1,31 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-	patientFirstName: 'Bob',
+
 	clientLastName: 'Bobbertson',
 	clientFirstName: 'Fred',
 	clientAddress: '22554 48th Ave NW Edmonton Alberta, Canada',
 	clientPhoneNumber: '666-666-6666',
 	clientEmail: '123dd@5d5dd.ca',
 
-	clientDocumentLICO: 'Yes',
-	clientDocumentAISH: 'Yes',
-	clientDocumentSA: 'Yes',
-	clientNotes: 'Smells bad?'
+	clientDocumentLICO: 'Confirmed',
+	clientDocumentAISH: 'Confirmed',
+	clientDocumentSA: 'Confirmed',
+	clientNotes: 'Smells bad?',
+
+	clientAlternativeCName: 'Jack',
+	clientAlternativeCAddress: '12252 92nd Ave Edmonton, Alberta, Canada',
+	clientAlternativeCPhone: '123-456-7890',
+	clientAlternativeCSPhone: '999-999-9999',
+	clientAlternativeCEmail: 'efijo@foji.cdoji',
+
+	patientFirstName: 'Bob',
+	patientSpecies: 'Doggo',
+	patientGender: 'M',
+	patientStatus: 'Neutered',
+	patientAge: '10',
+	patientColor: 'Brown spotted',
+	patientTatoo: '12352',
+	patientMicrochip: '11102'
 
 })
