ecb9b58fec2640fdbb6f03ed06b16a408435f5e6
MarcOlivierLajeunesse <malajeun@ualberta.ca>
2017-02-22 13:50:48 -0700

    made a controller for the search bar

--- app/controllers/client-list.js
+++ app/controllers/client-list.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+	actions: {
+		filterClientName(param) {
+			if (param !== '') {
+				return this.get('store').querry('client', {name:param});
+			} else {
+				return this.get('store').findAll('client');
+			}
+		}
+	}
 });
