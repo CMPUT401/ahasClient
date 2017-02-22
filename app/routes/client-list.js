4320151c737d1cd73a1c7706aa20340d0efd4e5b
MarcOlivierLajeunesse <malajeun@ualberta.ca>
2017-02-22 14:20:06 -0700

    disabled search bar, trying to send a get request to the server

--- app/routes/client-list.js
+++ app/routes/client-list.js
@@ -5,42 +5,45 @@ export default Ember.Route.extend(AuthenticatedRouteMixin ,{
 	ajax: Ember.inject.service(),
 	model(){
 		// TODO GET model with AJAX instead of using dummy data
-		/*
-		let ajaxGet = this.get('ajax').get('/api/client' , {
-			type: 'application/json',
-			data: {client: {
-				name: ""
-			}}, 
-		}.then(function(data){
+		var self = this;
+		let ajaxGet = this.get('ajax').request('/api/client' , {
+			//type: 'application/json'
+		}).then(function(data){
 			//console.log("name is " + cName);
 			console.log("status is " + JSON.stringify(data));
 			self.transitionToRoute('login');
 		},
 		function(data){
 			console.log("status is " + JSON.stringify(data));
+			// if (data === false){
+			// 	if (self.get('session.isAuthenticated')){
+			// 		self.get('session').invalidate();
+			// 		}
+			// 	self.transitionToRoute('/unauthorized');
+			// }
 		});
-		return ajaxGet.JSON.stringify(data);
-		*/
-		return [{
-			name: 'Boby'
-		},
-		{
-			name: 'Sally'
-		},
-		{
-			name: 'Jonathan'
-		},
-		{
-			name: 'Erica'
-		},
-		{
-			name: 'Joe'
-		},
-		{
-			name: 'Frank'
-		},
-		{
-			name: 'Francine'
-		}];
+		return ajaxGet;
+		
+		// return [{
+		// 	name: 'Boby'
+		// },
+		// {
+		// 	name: 'Sally'
+		// },
+		// {
+		// 	name: 'Jonathan'
+		// },
+		// {
+		// 	name: 'Erica'
+		// },
+		// {
+		// 	name: 'Joe'
+		// },
+		// {
+		// 	name: 'Frank'
+		// },
+		// {
+		// 	name: 'Francine'
+		// }];
 	}
 });
