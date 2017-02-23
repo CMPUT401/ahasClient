f49bbf8818eb90aade3672fa20ff60a815bb9ed0
erichsueh <erichsueh1337@hotmail.com>
2017-02-23 00:49:48 -0700

    Finished off view patient, patient is done but client isn't

--- app/routes/view-patient.js
+++ app/routes/view-patient.js
@@ -14,17 +14,21 @@ export default Ember.Route.extend(AuthenticatedRouteMixin,
 			).then(function(data){
 				Ember.run.later(function() {
        			 resolve({ id: JSON.stringify(data.patient.id),
-						   name: JSON.stringify(data.patient.name)
+						   name: JSON.stringify(data.patient.name),
+						   species: JSON.stringify(data.patient.species),
+						   age: JSON.stringify(data.patient.age),
+						   colour: JSON.stringify(data.patient.colour),
+						   tattoo: JSON.stringify(data.patient.tattoo),
+						   microchip: JSON.stringify(data.patient.microchip),
+						   status: JSON.stringify(data.patient.reproductive_status),
+						   gender: JSON.stringify(data.patient.gender)
 					
 				
 				});
-					console.log("we getdont here"); 
+					//console.log("we getdont here"); 
     		  });
                 console.log("status is " + JSON.stringify(data));
 				console.log("status is " + JSON.stringify(data.patient.name));
-				
-				 //self.set('patient', data.patient);
-				 //return[JSON.stringify(data.patient)];
 			},
 			function(data){
 				if (data === false){
@@ -37,28 +41,3 @@ export default Ember.Route.extend(AuthenticatedRouteMixin,
 	
 
 });
-
-/*
-    ajax: Ember.inject.service(),
-	beforeModel() {
-		var self = this;
-		console.log("patient extracted");
-		return this.get('ajax').request('/api/patients/1'
-			).then(function(data){
-                console.log("status is " + JSON.stringify(data));
-				//return(data.patient); //need to find way to extract patient portion of request....
-			},
-			function(data){
-				console.log("status is " + JSON.stringify(data));
-			});
-        
-		//return [ajaxGet];
-	},
-
-	setupControler(controller,beforeModel){
-		console.log(beforeModel.msg);
-	}
-
-});
-
-*/
