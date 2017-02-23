afb2682c4ef91a99249550e1497b70c44c6ecf9f
erichsueh <erichsueh1337@hotmail.com>
2017-02-22 20:07:39 -0700

    added kristy's code in, it works as intended

--- app/routes/view-patient.js
+++ app/routes/view-patient.js
@@ -1,51 +1,49 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
-import RSVP from 'rsvp';
+//import RSVP from 'rsvp';
 
-export default Ember.Route.extend(AuthenticatedRouteMixin,{
+export default Ember.Route.extend(AuthenticatedRouteMixin,
 
-	/*ajax: Ember.inject.service(),
-	model() {
-		//var self = this;
-		console.log("heh");
-		return new RSVP.Promise(function(resolve) {
-			console.log('heh2');
-			var self = this;
-			Ember.run.later(function() {
-				
-				return[self.get('ajax').request('/api/patients/1')];
-				//resolve({
-				//	msg: 'hold'
-					//this.get('ajax').request('/api/patients/1');
-					/*
-					var self = this;
-					var ajaxGet = this.get('ajax').request('/api/patients/1'
-						).then(function(data){
-                			console.log("status is " + JSON.stringify(data));
-							//return(data.patient); //need to find way to extract patient portion of request....
-						},
-						function(data){
-							console.log("status is " + JSON.stringify(data));
-						});
-       				console.log("patient extracted", ajaxGet);
-					return [ajaxGet];
-				//});
-			}, 3000);
-		});
-	},
-
-	setupController(controller,model){
-		console.log(model.msg);
-	}
-
-});*/
-
-
+	{
 
     ajax: Ember.inject.service(),
 	model() {
 		var self = this;
-		var ajaxGet = this.get('ajax').request('/api/patients/1'
+		var ajaxGet = new Ember.RSVP.Promise((resolve) =>
+		this.get('ajax').request('/api/patients/1'
+			).then(function(data){
+				Ember.run.later(function() {
+       			 resolve({ id: JSON.stringify(data.patient.id),
+						   name: JSON.stringify(data.patient.name)
+					
+				
+				});
+					console.log("we getdont here"); 
+    		  });
+                console.log("status is " + JSON.stringify(data));
+				console.log("status is " + JSON.stringify(data.patient.name));
+				
+				 //self.set('patient', data.patient);
+				 //return[JSON.stringify(data.patient)];
+			},
+			function(data){
+				if (data === false){
+				self.transitionTo('/unauthorized');
+				console.log("status is " + JSON.stringify(data));
+				}
+		}));
+		return(ajaxGet);
+	},
+	
+
+});
+
+/*
+    ajax: Ember.inject.service(),
+	beforeModel() {
+		var self = this;
+		console.log("patient extracted");
+		return this.get('ajax').request('/api/patients/1'
 			).then(function(data){
                 console.log("status is " + JSON.stringify(data));
 				//return(data.patient); //need to find way to extract patient portion of request....
@@ -53,13 +51,14 @@ export default Ember.Route.extend(AuthenticatedRouteMixin,{
 			function(data){
 				console.log("status is " + JSON.stringify(data));
 			});
-        console.log("patient extracted", ajaxGet);
-		return [ajaxGet];
+        
+		//return [ajaxGet];
 	},
 
-	setupControler(controller,model){
-		console.log(model.msg);
+	setupControler(controller,beforeModel){
+		console.log(beforeModel.msg);
 	}
 
 });
 
+*/
