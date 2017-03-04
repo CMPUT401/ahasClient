c08a8ce691e4215454274513fb0737a204d64679
Kristy <knewbury@ualberta.ca>
2017-03-04 13:16:25 -0800

    testing get all

--- app/controllers/search-contacts.js
+++ app/controllers/search-contacts.js
@@ -5,7 +5,7 @@ export default Ember.Controller.extend({
     actions: {
         getContact(contact){
             console.log("we get here", contact);
-            this.transitionToRoute('/view-contact/'+contact);
+            //this.transitionToRoute('/view-contact/'+contact);
         }
     }
 });
--- app/routes/search-contacts.js
+++ app/routes/search-contacts.js
@@ -1,9 +1,34 @@
 import Ember from 'ember';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default Ember.Route.extend({
-    model() {
-        return{
-        id: "1"
-        };
-    }
+export default Ember.Route.extend(AuthenticatedRouteMixin,
+	{
+    session: Ember.inject.service(),
+    ajax: Ember.inject.service(),
+	model() {
+		var self = this;
+		var ajaxGet = new Ember.RSVP.Promise((resolve) =>
+		this.get('ajax').request('/api/contacts'
+			).then(function(data){
+				Ember.run(function() {
+       			 resolve({ 
+                           overall:  JSON.stringify(data),
+                           status: JSON.stringify(data.success),
+						   data: JSON.stringify(data.contacts)
+				
+				});
+    		  });
+			
+			},
+			function(data){
+				if (response === false){
+					if (self.get('session.isAuthenticated')){
+						self.get('session').invalidate();
+							}
+					self.transitionToRoute('/unauthorized');
+            }
+		}));
+		return(ajaxGet);
+	},
+	
 });
