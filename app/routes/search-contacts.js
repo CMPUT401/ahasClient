d8e449aff02010fe707988524cd0368fe0e96fe9
Kristy <knewbury@ualberta.ca>
2017-03-05 13:17:21 -0800

    still working on displaying all contacts on search page

--- app/routes/search-contacts.js
+++ app/routes/search-contacts.js
@@ -6,14 +6,16 @@ export default Ember.Route.extend(AuthenticatedRouteMixin,
     session: Ember.inject.service(),
     ajax: Ember.inject.service(),
 	model() {
-		/*var self = this;
+		var self = this;
 		var ajaxGet = new Ember.RSVP.Promise((resolve) =>
 		this.get('ajax').request('/api/contacts'
 			).then(function(data){
 				Ember.run(function() {
+					console.log(data, JSON.stringify(data.contacts));
        			 resolve({ 
-
-						   contacts: JSON.stringify(data.contacts)
+						  
+						  contacts: JSON.stringify(data.contacts),
+						  
 				
 				});
     		  });
@@ -27,9 +29,8 @@ export default Ember.Route.extend(AuthenticatedRouteMixin,
 					self.transitionToRoute('/unauthorized');
             }
 		}));
-		//return(ajaxGet);*/
-		return 
-			[{id:1,"first_name":"Jonah","last_name":"Smith"},{id:2,"first_name":"Ryan","last_name":"Jones"},{id:3,"first_name":"Fran","last_name":"Stoker"}];
-	},
+		return(ajaxGet);
+		
+	}
 	
 });
