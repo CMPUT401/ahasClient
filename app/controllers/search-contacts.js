1dfcac15d5f86345dcbbd5102d7f11dc638abfb5
Kristy <knewbury@ualberta.ca>
2017-03-08 02:18:21 -0800

    possible progress towards search

--- app/controllers/search-contacts.js
+++ app/controllers/search-contacts.js
@@ -6,6 +6,16 @@ export default Ember.Controller.extend({
         getContact(contact){
             console.log("we get here", contact);
             this.transitionToRoute('/view-contact/'+contact);
+        },
+        filterContact(model){
+            var lol = document.getElementsByClassName('veterinariansContact');
+           //just clear all for now, conservative in clearing all. maybe not necessary
+           //guess only want to do this if the query is valid.... lol 
+           Ember.$('.veterinariansContact').html('<div></div>');
+           Ember.$('.volunteersContact').html('<div></div>');
+           Ember.$('.laboratoryContact').html('<div></div>');
+
+            console.log(JSON.stringify(model) ,document.getElementById('search-bar').value , lol);
         }
     }
 });
