74979a8c58ef4763a7ce14067798975a3ccb32bf
MarcOlivierLajeunesse <malajeun@ualberta.ca>
2017-03-08 14:22:41 -0700

    cleaned up some code

--- app/routes/client-list.js
+++ app/routes/client-list.js
@@ -4,36 +4,13 @@ import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-rout
 export default Ember.Route.extend(AuthenticatedRouteMixin ,{
 	ajax: Ember.inject.service(),
 	model(){
-		// TODO GET model with AJAX instead of using dummy data
 		var self = this;
-		// let ajaxGet = this.get('ajax').request('/api/client' , {
-		// 	// type: 'application/json',
-		// 	dataType: 'application/json',
-		// 	// data: {client: {
-		// 	// 	firstName: '',
-		//  //  lastName: '',
-		// 	// 	id: ''
-		// 	// }}
-		// 	//method: 'GET'
-		// }).then(function(data){
-		// 	//console.log("name is " + cName);
-		// 	console.log("success status is " + JSON.stringify(data));
-		// },
-		// function(data){
-		// 	console.log("error status is " + JSON.stringify(data));
-		// 	if (data === false){
-		// 		if (self.get('session.isAuthenticated')){
-		// 			self.get('session').invalidate();
-		// 			}
-		// 		self.transitionToRoute('/unauthorized');
-		// 	}
-		// });
 
 		let ajaxGet = new Ember.RSVP.Promise((resolve) =>
 		this.get('ajax').request('/api/client'
 			).then(function(data){
 				Ember.run(function() {
-					console.log("data is " + JSON.stringify(data), JSON.stringify(data.client));
+					// console.log("data is " + JSON.stringify(data), JSON.stringify(data.client));
 					resolve({ 
 						clients: deserialAttributes(data.clients),
 
@@ -58,44 +35,6 @@ export default Ember.Route.extend(AuthenticatedRouteMixin ,{
 		// console.log("ajax is " + ajaxGet);
 		// return this.get('store').findAll('client-list');
 		return ajaxGet;
-		
-		// return {
-		// 	clients: [{
-		// 		firstName: 'Boby',
-		// 		lastName: 'Oor',
-		// 		id :110
-		// 	},
-		// 	{
-		// 		firstName: 'Sally',
-		// 		lastName: 'Stenson',
-		// 		id :111
-		// 	},
-		// 	{
-		// 		firstName: 'Jonathan',
-		// 		lastName: 'Lafleur',
-		// 		id :112
-		// 	},
-		// 	{
-		// 		firstName: 'Erica',
-		// 		lastName: 'Smith',
-		// 		id :113
-		// 	},
-		// 	{
-		// 		firstName: 'Joe',
-		// 		lastName: 'Blo',
-		// 		id :114
-		// 	},
-		// 	{
-		// 		firstName: 'Frank',
-		// 		lastName: 'Wurst',
-		// 		id :115
-		// 	},
-		// 	{
-		// 		firstName: 'Francine',
-		// 		lastName: 'Mancine',
-		// 		id :116
-		// 	}]
-		// };
 	}
 });
 
