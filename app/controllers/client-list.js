623e1b2480eb5ef5267d21f8e34418b9f7b30eaf
MarcOlivierLajeunesse <malajeun@ualberta.ca>
2017-03-08 14:47:28 -0700

    controller fetches client id

--- app/controllers/client-list.js
+++ app/controllers/client-list.js
@@ -2,28 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 	actions: {
-		filterClientName(param) {
-			if (param !== '') {
-				var queryResult = this.get('store').query('client-list', {firstName:param});
-				console.log("query result is " + queryResult);
-				return queryResult;
-			} else {
-				return this.get('store').findAll('client-list');
-			}
-		}
-	},
-	clients: function() {
-        var model = this.get('model');
-        var search = this.get('search');
-        var current = 1;
-        var clients = model.filter(function(item) {
-            var reg = '.*'+search.toLowerCase()+'.*';
-            var name = item.get('firstName').toLowerCase();
-            if(name.match(reg) && current < 20) { 
-                current++;
-                return true; 
-            }
-        });
-        return clients;
-    }.property('search','model.length')
+		viewClient: function(clientID){
+            console.log(clientID);
+        }
+
+	}
 });
