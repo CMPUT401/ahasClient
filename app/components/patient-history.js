938b04426db54f563fe374d524f85b7b31b38b7b
MarcOlivierLajeunesse <malajeun@ualberta.ca>
2017-03-09 11:34:20 -0700

    made action to toggle component visibility

--- app/components/patient-history.js
+++ app/components/patient-history.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+	isVisible: true,
+	actions:{
+		newEntry: function(){
+			console.log("making a new medical history entry");
+		},
+		toggleVisibility: function(){
+			this.toggleProperty('isVisible');
+		}
+	}
 });
