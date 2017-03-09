8f54936589fec007604bc73e072e7d3848a73b31
erichsueh <erichsueh1337@hotmail.com>
2017-03-09 00:18:42 -0700

    hmm random 200 OK status

--- app/routes/view-side-note.js
+++ app/routes/view-side-note.js
@@ -6,31 +6,24 @@ export default Ember.Route.extend(AuthenticatedRouteMixin,
 
 	
 	{
-	/*
+	
     ajax: Ember.inject.service(),
 	model() {
 		var self = this;
 		var ajaxGet = new Ember.RSVP.Promise((resolve) =>
-		this.get('ajax').request('/api/patients/1'
+		this.get('ajax').request('/api/patients/1/medical_records'
 			).then(function(data){
 				Ember.run.later(function() {
-       			 resolve({ id: JSON.stringify(data.patient.id).replace(/\"/g, ""),
-						   name: JSON.stringify(data.patient.name).replace(/\"/g, ""),
-						   species: JSON.stringify(data.patient.species).replace(/\"/g, ""),
-						   age: JSON.stringify(data.patient.age).replace(/\"/g, ""),
-						   colour: JSON.stringify(data.patient.colour).replace(/\"/g, ""),
-						   tattoo: JSON.stringify(data.patient.tattoo).replace(/\"/g, ""),
-						   microchip: JSON.stringify(data.patient.microchip).replace(/\"/g, ""),
-						   status: JSON.stringify(data.patient.reproductive_status).replace(/\"/g, ""),
-						   client_id: JSON.stringify(data.patient.client_id).replace(/\"/g, ""),
-						   gender: JSON.stringify(data.patient.gender).replace(/\"/g, "")
+       			 resolve({ id: JSON.stringify(data.appointment.id).replace(/\"/g, ""),
+						   notes: JSON.stringify(data.appointment.note).replace(/\"/g, ""),
+						   species: JSON.stringify(data.appointment.sig).replace(/\"/g, ""),
 				});
 				console.log("we getdont here");
 				//this.get('ajax').request('/api/client/1');
 
     		  });
                 console.log("status is " + JSON.stringify(data));
-				console.log("status is " + JSON.stringify(data.patient.name));
+				console.log("status is " + JSON.stringify(data.appointment.sig));
 			},
 			function(data){
 				if (data === false){
@@ -41,6 +34,6 @@ export default Ember.Route.extend(AuthenticatedRouteMixin,
 		return(ajaxGet);
 
 	},
-	*/
+	
 
 });
