164cf3ac07dec5604563cd9e94213e1552d3e13e
erichsueh <erichsueh1337@hotmail.com>
2017-03-10 05:50:31 -0700

    gona work on test cases and call it a night

--- tests/acceptance/new-patient-test.js
+++ tests/acceptance/new-patient-test.js
@@ -1,13 +1,24 @@
 import { test } from 'qunit';
 import moduleForAcceptance from 'ahasweb/tests/helpers/module-for-acceptance';
+import { authenticateSession, invalidateSession } from '../helpers/ember-simple-auth';
 
-moduleForAcceptance('Acceptance | create patient');
+moduleForAcceptance('Acceptance | patient info');
 
-test('visiting /new-patient', function(assert) {
-  visit('/new-patient');
+test('visiting /view-patient/1 before login', function(assert) {
+  invalidateSession(this.application);
+  visit('view-patient/1');
+
+  andThen(function(){
+    assert.notEqual(currentURL(), '/view-patient/1');
+  });
+});
+
+test('visiting /view-patient/1', function(assert) {
+  authenticateSession(this.application);
+  visit('/view-patient/1');
 
   andThen(function() {
-    assert.equal(currentURL(), '/new-patient');
+    assert.equal(currentURL(), '/view-patient/1');
   });
 });
 
