5b03296f55f0b1663534bb6bee682eeb4ae86e96
erichsueh <erichsueh1337@hotmail.com>
2017-03-10 06:21:33 -0700

    please work

--- app/routes/new-side-note.js
+++ app/routes/new-side-note.js
@@ -1,7 +1,6 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-<<<<<<< HEAD
 export default Ember.Route.extend({
 	model(param){
 		console.log(param.IDs[0]);
