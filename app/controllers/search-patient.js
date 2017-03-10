8e45ea5cb801bb047ad4ffbdd4c494d29b2abb53
erichsueh <erichsueh1337@hotmail.com>
2017-03-10 01:06:14 -0700

    finished search patient everything works

--- app/controllers/search-patient.js
+++ app/controllers/search-patient.js
@@ -10,7 +10,7 @@ export default Ember.Controller.extend({
         filterPatient: function(){
             var input = document.getElementById('search-bar').value.trim();
             if(input === "" || input === undefined){
-                this.set('model.patientFiltered', this.get(model.patient));
+                this.set('model.patientFiltered', this.get(model.patients));
             }
             else {
                 filter(input, this.get('model'), this);
@@ -22,9 +22,9 @@ export default Ember.Controller.extend({
 
 function filter(input, model, self){
     var results = [];
-    for(var i = 0; i < model.patient.length; i++){
+    for(var i = 0; i < model.patients.length; i++){
         if(input === model.patients[i].first_name || input === model.patients[i].last_name){
-            var patients = {
+            var patient = {
                             first_name: model.patients[i].first_name,
                             last_name: model.patients[i].last_name,
                             id: model.patients[i].id
