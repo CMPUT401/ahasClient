7bb75430238d8c472b5d262ca9cd5bacebac29c8
MarcOlivierLajeunesse <malajeun@ualberta.ca>
2017-03-09 18:44:30 -0700

    now sends the patient id down to the nested components

--- app/components/history-container.js
+++ app/components/history-container.js
@@ -3,14 +3,19 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 	chronoIsVisible: true,
 	medicationIsVisible: false,
+	patientID: 0,
+	// init(patientId){
+	// 	//this.set('patientID', patientId);
+	// 	this._super(...arguments);
+	// },
 	actions: {
-		showChronological: function(patientId){
-			console.log("show chrono, the id is " + patientId);
+		showChronological: function(){
+			// console.log("show chrono, the id is " + patientId);
 			this.set('chronoIsVisible', true);
 			this.set('medicationIsVisible', false);
 		},
-		showMedication: function(patientId){
-			console.log("show medication, the id is " + patientId);
+		showMedication: function(){
+			// console.log("show medication, the id is " + patientId);
 			this.set('chronoIsVisible', false);
 			this.set('medicationIsVisible', true);
 		}
--- app/components/medication-history.js
+++ app/components/medication-history.js
@@ -2,11 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 	isVisible: false,
+	patientId:0 ,
 	actions:{
 		newEntry: function(){
 			console.log("making a new medical history entry");
 		},
 		toggleVisibility: function(){
+			// console.log("show medication, the id is " + patientId);
 			if(this.get('isVisible')){
 				this.set('isVisible', false);
 			} else {
