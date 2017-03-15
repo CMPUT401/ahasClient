ae0f0e46d7675afb8ab3b3c63879ec32a8ac776a
erichsueh <erichsueh1337@hotmail.com>
2017-03-15 11:09:23 -0600

    added semicolon

--- app/controllers/new-side-note.js
+++ app/controllers/new-side-note.js
@@ -26,8 +26,8 @@ export default Ember.Controller.extend({
 		
 			});
 			ajaxPost.then(function(data){
-				console.log("status is " + JSON.stringify(data));
-				self.transitionTo('/api/patients/1/medical_records/1')
+				console.log("status is " + data);
+				self.transitionTo('/api/patients/1/medical_records/1');
 			},
 			function(response){
 				if (response === false){
