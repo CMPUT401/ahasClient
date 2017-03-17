14009206fb24bcc24ac368691e041ded084821be
MarcOlivierLajeunesse <malajeun@ualberta.ca>
2017-03-17 15:28:40 -0600

    checking browser support for File APIs

--- app/routes/lab-result-upload.js
+++ app/routes/lab-result-upload.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	checkFileApiSupport()
 });
+
+function: checkFileApiSupport(){
+	if (window.File && window.FileReader && window.FileList && window.Blob){
+		console.log("File API is supported");
+		return true;
+	} else{
+		console.log("File API is not supported");
+		alert("The File APIs are not fully supported in this browser. ");
+		return false;
+	}
+}
