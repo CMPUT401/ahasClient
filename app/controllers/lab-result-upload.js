711d37e3abdb9398e48763982693039df77fa548
MarcOlivierLajeunesse <malajeun@ualberta.ca>
2017-03-18 15:35:35 -0600

    controller now stores the file so that the send button can access it. bad design, but it works

--- app/controllers/lab-result-upload.js
+++ app/controllers/lab-result-upload.js
@@ -1,16 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+	loadedFile: null,
 	actions: {
 		fileLoaded: function(file){
+			this.set('loadedFile', file);
 			console.log("name is " + file.name);
 			console.log("type is " + file.type);
 			console.log("data is ");
 			console.log(file.data);
 			console.log("size is " + file.size + " bytes");
 		},
-		sendFile:function(file){
-			console.log("uploading file " + file.name);
+		sendLabResults: function(){
+			console.log("uploading file " + this.loadedFile.name);
 		}
 	}
 });
