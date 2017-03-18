af247945fae1a4ba4c6c984752763be3ffa97715
MarcOlivierLajeunesse <malajeun@ualberta.ca>
2017-03-17 18:20:51 -0600

    trying with ember-cli-file-picker

--- app/controllers/lab-result-upload.js
+++ app/controllers/lab-result-upload.js
@@ -1,24 +1,25 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-	actions: {
-		clickChooseFile: function(){
-			console.log("click choose file");
-			document.getElementById('files').addEventListener('change', handleFileSelect, false);
-			return true;
-		}
-	}
+	// actions: {
+	// 	clickChooseFile: function(){
+	// 		console.log("click choose file");
+	// 		document.getElementById('files').addEventListener('change', handleFileSelect, false);
+	// 		return true;
+	// 	}
+	// }
 });
-function handleFileSelect(evt){
-	console.log("handling file select");
-	var files = evt.target.files; // FileList object of File objects
+// function handleFileSelect(evt){
+	// from https://www.html5rocks.com/en/tutorials/file/dndfiles/
+// 	console.log("handling file select");
+// 	var files = evt.target.files; // FileList object of File objects
 
-	var output = [];
-	for(var i = 0, f; f = files[i]; i++){
-		output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
-			f.size, 'bytes, last modified: ', f.lastModifiedDate ? 
-			f.lastModifiedDatetoLocalDateString(): 'n/a', '</li>');
-	}
-	document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
-	return true;
-}
+// 	var output = [];
+// 	for(var i = 0, f; f = files[i]; i++){
+// 		output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
+// 			f.size, 'bytes, last modified: ', f.lastModifiedDate ? 
+// 			f.lastModifiedDatetoLocalDateString(): 'n/a', '</li>');
+// 	}
+// 	document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
+// 	return true;
+// }
