1f651aa11a44d6542897d29d6337513c086fde68
MarcOlivierLajeunesse <malajeun@ualberta.ca>
2017-03-18 20:34:32 -0600

    now sends a custom date, selected from the date picker

--- app/controllers/lab-result-upload.js
+++ app/controllers/lab-result-upload.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 	loadedFile: null,
+	session: Ember.inject.service(),
 	ajax: Ember.inject.service(),
 	actions: {
 		fileLoaded: function(file){
@@ -16,13 +17,15 @@ export default Ember.Controller.extend({
 			console.log("uploading file " + this.loadedFile.name + 
 				" " + this.loadedFile.data);
 			console.log("patient id is " + patientId);
+			console.log("date is " + this.get('datePicker'));
 			var self = this;
 			let ajaxPost = this.get('ajax').post('api/lab-results/' + patientId, {
 				type: 'application/json',
 				data: {lab_result: {
 					file_name: this.loadedFile.name,
 					image: this.loadedFile.data,
-					date: new Date()
+					// date: new Date()
+					date: this.get('datePicker')
 				}},
 			}).then(function(response){
 				console.log("status is " +JSON.stringify(response));
