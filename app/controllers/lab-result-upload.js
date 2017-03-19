73b05519f288ae274f5b0e7e6e315acde46be78f
MarcOlivierLajeunesse <malajeun@ualberta.ca>
2017-03-18 19:53:58 -0600

    working on ajax post request

--- app/controllers/lab-result-upload.js
+++ app/controllers/lab-result-upload.js
@@ -16,10 +16,27 @@ export default Ember.Controller.extend({
 			console.log("uploading file " + this.loadedFile.name + 
 				" " + this.loadedFile.data);
 			console.log("patient id is " + patientId);
-			// var = self;
-			// let ajaxPost = this.get('ajax').post('api/lab', {
-			// 	type
-			// })
+			var = self;
+			let ajaxPost = this.get('ajax').post('api/lab-results' + patientId, {
+				type: 'application/json',
+				data: {lab_result: {
+					file_name: this.loadedFile.name,
+					image: this.loadedFile.data,
+					date: new Date()
+				}},
+			}).then(function(response){
+				console.log("status is " +JSON.stringify(response));
+
+			}, then(response){
+				console.log("status is " + JSON.stringify(response));
+				if(response === false){
+					if(self.get('session.isAuthenticated')){
+						self.get('session').invalidate();
+					}
+					self.transitionToRoute('/login');
+				}
+			});
+			return ajaxPost;
 		}
 	}
 });
