765df644abec8e2d3028830956a5b87265a791d0
MarcOlivierLajeunesse <malajeun@ualberta.ca>
2017-03-20 22:12:34 -0600

    started working on ajax request. added service injections

--- app/components/medication-history.js
+++ app/components/medication-history.js
@@ -3,6 +3,9 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 	isVisible: false,
 	patientId:0 ,
+	ajax: Ember.inject.service(),
+	medicalRecord: [],
+	router: Ember.inject.service('-routing'),
 	actions:{
 		newEntry: function(){
 			console.log("making a new medical history entry");
@@ -19,5 +22,30 @@ export default Ember.Component.extend({
 			//this.get('router').transitionTo('view-medical-record', [this.patientId, recordID]);
 			console.log('view entry ' + recordID);
 		}
+	},
+	init(){
+		this._super(...arguments);
+		console.log("calling ajax");
+		var self = this;
+		var ajaxGet = new Ember.RSVP.Promise((resolve) =>
+			this.get('ajax').request('api/patients/' + this.patientId + '/medication'
+				).then(function(data){
+					console.log("data is" + JSON.stringify(data));
+					Ember.run(function(){
+						resolve({
+							history: deserialAttributes(data.medical_records)
+						});
+						// console.log(deserialAttributes(data.medical_records));
+						self.set('medicalRecord', deserialAttributes(data.medical_records));
+					});
+				},
+				function(data){
+					if (data === false){
+						// self.transitionTo('/unauthorized');
+						console.log("status is " + JSON.stringify(data));
+					}		
+				})
+		);
+		console.log(this.medicalRecord);
 	}
 });
