d827e68b769be34db2c3defed401684ef9295b70
Justin Barclay <justincbarclay@gmail.com>
2017-03-21 11:54:44 -0600

    stylize and add user to admin table

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,4 +1,5 @@
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+import Ember from 'ember';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin ,{
 	session: Ember.inject.service(),
@@ -31,11 +32,10 @@ export default Ember.Route.extend(AuthenticatedRouteMixin ,{
 function deserialAttributes(users){
 	var deserial = [];
 	for(var i = 0; i < users.length; i++) {
-		var user = {}
-		console.log(users[i])
-		user.id = users[i].id
-		user.email = users[i].email
-		user.name = users[i].name
+		var user = {};
+		user.id = users[i].id;
+		user.email = users[i].email;
+		user.name = users[i].name;
 		deserial.push(user);
 	}
 	return(deserial);
