223202296af72df32858797c19c041bbb3c31008
MarcOlivierLajeunesse <malajeun@ualberta.ca>
2017-03-21 21:01:21 -0600

    made a save client button

--- app/controllers/edit-client.js
+++ app/controllers/edit-client.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+	session: Ember.inject.service(),
+	ajax: Ember.inject.service(),
+	actions:{
+		saveClient: function(){
+			console.log("saving client!");
+		}
+	}
 });
