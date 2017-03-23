6a9d48ca7bf8edb23c699bd8e3113fc85ff6e9b7
MarcOlivierLajeunesse <malajeun@ualberta.ca>
2017-03-23 11:28:44 -0600

    made upload radiograph button. trasitions to the upload radiography page.

--- app/components/radiography-history.js
+++ app/components/radiography-history.js
@@ -3,4 +3,19 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 	isVisible: false,
 	patientId: 0,
+	ajax: Ember.inject.service(),
+	radiographyList: [],
+	router: Ember.inject.service('-routing'),
+	actions: {
+		uploadResult: function(){
+			// console.log("making a new medical history entry");
+			this.get('router').transitionTo('radiography-upload', [this.patientId]);
+		},
+		viewEntry: function(radiographyID){
+			console.log('view radiograph ' + radiographyID);
+			// TODO transition to lab result
+			//this.get('router').transitionTo('view-image-record', [this.patientId, labResultID]);
+		}
+	},
+
 });
