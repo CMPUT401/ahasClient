41a7566ed5fb87d954daff7ee6521d6bb01a5003
MarcOlivierLajeunesse <malajeun@ualberta.ca>
2017-03-22 19:44:16 -0600

    added action to the controller to enable file picker

--- app/controllers/radiography-upload.js
+++ app/controllers/radiography-upload.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+	loadedFile: null,
+	session: Ember.inject.service(),
+	ajax: Ember.inject.service(),
+	actions: {
+		fileLoaded: function(file){
+			this.set('loadedFile', file);
+			// console.log("name is " + file.name);
+			// console.log("type is " + file.type);
+			// console.log("data is ");
+			// //console.log(file.data);
+			// console.log("size is " + file.size + " bytes");
+		}
+	}
 });
