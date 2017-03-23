fb897ac4048f0f058cfc5711e286f55256ea8e14
MarcOlivierLajeunesse <malajeun@ualberta.ca>
2017-03-22 20:15:40 -0600

    made upload button. began working on component javascript

--- app/components/lab-result-history.js
+++ app/components/lab-result-history.js
@@ -2,5 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 	isVisible: false,
-	patientId:0 ,
+	patientId: 0,
+	ajax: Ember.inject.service(),
+	labResultList: [],
+	router: Ember.inject.service('-routing'),
+	actions: {
+		uploadResult: function(){
+			console.log("making a new medical history entry");
+		}
+	}
 });
