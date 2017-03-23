6645f741eaaec06d7c76fde9123ab8a9fc600997
MarcOlivierLajeunesse <malajeun@ualberta.ca>
2017-03-22 20:09:48 -0600

    user can now hide and show the component as needed

--- app/components/history-container.js
+++ app/components/history-container.js
@@ -3,17 +3,25 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 	chronoIsVisible: true,
 	medicationIsVisible: false,
+	labResultIsVisible:false,
 	patientID: 0,
 	actions: {
 		showChronological: function(){
 			// console.log("show chrono, the id is " + patientId);
 			this.set('chronoIsVisible', true);
 			this.set('medicationIsVisible', false);
+			this.set('labResultIsVisible', false);
 		},
 		showMedication: function(){
 			// console.log("show medication, the id is " + patientId);
 			this.set('chronoIsVisible', false);
 			this.set('medicationIsVisible', true);
+			this.set('labResultIsVisible', false);
+		},
+		showLabResults: function(){
+			this.set('chronoIsVisible', false);
+			this.set('medicationIsVisible', false);
+			this.set('labResultIsVisible', true);
 		}
 	}
 });
--- app/components/lab-result-history.js
+++ app/components/lab-result-history.js
@@ -1,4 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+	isVisible: false,
+	patientId:0 ,
 });
