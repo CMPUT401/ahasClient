9c4d2d051dbb16b1970dcbf0f25d53d4f150261b
MarcOlivierLajeunesse <malajeun@ualberta.ca>
2017-03-23 11:11:42 -0600

    can now hide and show radiography component

--- app/components/history-container.js
+++ app/components/history-container.js
@@ -3,7 +3,8 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 	chronoIsVisible: true,
 	medicationIsVisible: false,
-	labResultIsVisible:false,
+	labResultIsVisible: false,
+	radiographyIsVisible: false,
 	patientID: 0,
 	actions: {
 		showChronological: function(){
@@ -11,17 +12,27 @@ export default Ember.Component.extend({
 			this.set('chronoIsVisible', true);
 			this.set('medicationIsVisible', false);
 			this.set('labResultIsVisible', false);
+			this.set('radiographyIsVisible', false);
 		},
 		showMedication: function(){
 			// console.log("show medication, the id is " + patientId);
 			this.set('chronoIsVisible', false);
 			this.set('medicationIsVisible', true);
 			this.set('labResultIsVisible', false);
+			this.set('radiographyIsVisible', false);
 		},
 		showLabResults: function(){
 			this.set('chronoIsVisible', false);
 			this.set('medicationIsVisible', false);
 			this.set('labResultIsVisible', true);
+			this.set('radiographyIsVisible', false);
+		},
+		showRadiographs: function(){
+			this.set('chronoIsVisible', false);
+			this.set('medicationIsVisible', false);
+			this.set('labResultIsVisible', false);
+			this.set('radiographyIsVisible', true);
 		}
+
 	}
 });
