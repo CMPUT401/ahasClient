e9afbfa32c183ebf488a5c3e3e73597b05a96b45
MarcOlivierLajeunesse <malajeun@ualberta.ca>
2017-03-22 23:48:11 -0600

    making successful ajax request

--- app/routes/view-image-record.js
+++ app/routes/view-image-record.js
@@ -1,4 +1,32 @@
 import Ember from 'ember';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 export default Ember.Route.extend({
+	session: Ember.inject.service(),
+	ajax: Ember.inject.service(),
+	model(param){
+		var self = this;
+		//ajax get request to populate field
+		var ajaxGet = new Ember.RSVP.Promise((resolve) =>
+		this.get('ajax').request('/api/patients/' + param.patientID + "/images/" + param.imageID
+			).then(function(data){
+				console.log("data is " + JSON.stringify(data));
+				Ember.run(function() {
+				resolve({
+					
+				});
+			  });
+			
+			},
+			function(data){
+				if (data === false){
+					if (self.get('session.isAuthenticated')){
+					self.get('session').invalidate();
+					}
+				self.transitionTo('/login');
+					console.log("status is " + JSON.stringify(data));
+				}
+		}));
+		return ajaxGet;
+	}
 });
