f7db177c942814eae4d60fb9e3a6bf83bf8b666d
Kristy <knewbury@ualberta.ca>
2017-03-24 01:13:29 -0700

    forgot to rm dead code

--- app/controllers/view-patient.js
+++ app/controllers/view-patient.js
@@ -6,23 +6,3 @@ export default Ember.Controller.extend({
 }
 
 });
-
-function checkUpdate(date){
-
-    var day = date.getDay() ;
-    var month = date.getMonth()  ;
-    var year = date.getFullYear();
-
-    var current = new Date();
-
-    var currentDay = current.getDay() ;
-    var currentMonth = current.getMonth()  ;
-    var currentYear = current.getFullYear();
-    var currentHours = current.getHours();
-
-    //exact minute of midnight is when we will autofinalize
-    if (currentDay === day && currentMonth === month && currentYear === year && currentHours <= 24 ){
-        return(true);
-    }
-    return(false);
-}
