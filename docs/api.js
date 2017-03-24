22cbf60fd61567c01563ef4dde8848f215b498d1
erichsueh <erichsueh1337@hotmail.com>
2017-03-24 05:06:56 -0600

    added docs..?

--- docs/api.js
+++ docs/api.js
@@ -1,6 +1,28 @@
 YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
-    "classes": [],
+    "classes": [
+        "ClientInfoController",
+        "ClientListController",
+        "EditClientController",
+        "HistoryContainerComponentController",
+        "LabResultHistoryComponentController",
+        "List-Side-Note Controller",
+        "MedicationHistoryComponentController",
+        "NewClientController",
+        "PatientPictureComponentController",
+        "RadiographyHistoryComponentController",
+        "VaccineHistoryComponentController",
+        "labResultUploadController",
+        "new side note Controller",
+        "new-calendar Controller",
+        "new-patient controller",
+        "patientHistoryComponentController",
+        "radiographyUploadController",
+        "search-patient Controller",
+        "upload patient controleler",
+        "view appointment controller",
+        "view-patient"
+    ],
     "modules": [],
     "allModules": [],
     "elements": []
