00c9930286fe044e7d5488f0b52aa288e8aad2bb
erichsueh <erichsueh1337@hotmail.com>
2017-03-24 00:03:53 -0600

    starting the component, realise i dont have picture id on patient

---
+++ app/components/patient-picture.js
@@ -0,0 +1,36 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+	isVisible: false,
+	patientId:0 ,
+	ajax: Ember.inject.service(),
+	medicationList: [],
+	router: Ember.inject.service('-routing'),
+	init(){
+		this._super(...arguments);
+		console.log("calling ajax for medcation List");
+		var self = this;
+		var ajaxGet = new Ember.RSVP.Promise((resolve) =>
+			this.get('ajax').request('api/patients/' + this.patientId + '/medications'
+				).then(function(data){
+					console.log("data is" + JSON.stringify(data));
+					Ember.run(function(){
+						resolve({
+							// medications: deserialAttributes(data.medications)
+						});
+						// console.log(deserialAttributes(data.medical_records));
+						self.set('medicationList', deserialAttributes(data.medications));
+					});
+				},
+				function(data){
+					if (data === false){
+						// self.transitionTo('/unauthorized');
+						// self.get('router').transitionTo('unauthorized'); //not sure if this works
+						console.log("status is " + JSON.stringify(data));
+					}		
+				})
+		);
+		console.log(this.medicationList);
+		return(ajaxGet);
+	}
+});
