6671b67dd56c218c3a458aea7d37aa7499317c58
MarcOlivierLajeunesse <malajeun@ualberta.ca>
2017-03-24 01:25:00 -0600

    made docs for client-list

--- app/controllers/client-list.js
+++ app/controllers/client-list.js
@@ -1,7 +1,15 @@
 import Ember from 'ember';
 
+/**
+* Controller for client-list
+* @class ClientListController
+*/
 export default Ember.Controller.extend({
 	actions: {
+		/**
+		* fitlers clients according to user input
+		* @method filterClient
+		*/
 		filterClient: function(){
 			var input = document.getElementById('search-bar').value.trim();
 			if(input === "" || input === undefined){
@@ -11,10 +19,21 @@ export default Ember.Controller.extend({
 				filter(input, this.get('model'), this);
 			}
 		},
+		/**
+		* handles action called when user clicks on a client's name. Redirects to that client's
+		* info page. 
+		* @method viewClient
+		* *param {int} clientID The ID of the client
+		*/
 		viewClient: function(clientID){
             console.log(clientID);
             this.transitionToRoute("/client-info/" + clientID);
         },
+        /**
+        * handles action called when user clicks New Client button. 
+        * Redirects to the client page
+		* @method newPatient
+        */
         newClient: function(){
         	this.transitionToRoute("/new-client/");
         }
