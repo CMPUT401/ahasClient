65acedcfc1b89f5981bdb39f35085516e62a978f
MarcOlivierLajeunesse <malajeun@ualberta.ca>
2017-03-24 00:35:52 -0600

    finished docs for client-info. can't get it to display

--- app/controllers/client-info.js
+++ app/controllers/client-info.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 	actions: {
-		/**
+		/** 
 		* handles action called when user clicks New Patient button
 		* redirects to the client's new patient page
 		* @param {int} clientID The ID of the client 
@@ -14,18 +14,32 @@ export default Ember.Controller.extend({
 			});
 			//this.transitionTo('new-patient', { queryParams: { clientID: '1' }});
 		},
+		/** 
+		* handles the action called when user clicks on a patient's name
+		* redirects to the patient's view-patient page
+		* @param {int} petID The ID of the patient
+		*/
 		viewPatient: function(petId){
 			console.log(petId);
 			this.transitionToRoute("/view-patient/" + petId);
 		},
+		/**
+		* handles the action called when the user clicks the Edit Client button
+		* redirects to the client's endit-client page
+		* @param {int} clientID The ID of the client
+		*/
 		editClient: function(clientID){
 			this.transitionToRoute('/edit-client/' + clientID);
 		},
-
+		/**
+		* handles the action called when a user clicks the Make Appointment button
+		* redirects to the client's callendar
+		* @param {int} clientID The ID of the client
+		*/
 		newAppointment: function(clientID){
 			console.log(clientID);
 			this.transitionToRoute("/new-calendar/").then(function(newRoute){
-				newRoute.controller.set("c_ID",clientID)
+				newRoute.controller.set("c_ID",clientID);
 			});
 		}
 	}
