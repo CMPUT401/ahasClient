4796d2c334d07e4e5012f9a9f5d8e6d8ae45d7eb
Kristy <knewbury@ualberta.ca>
2017-04-05 13:36:42 -0700

    forgot to protect contact edit and create routes against manual transition

--- app/routes/new-contact.js
+++ app/routes/new-contact.js
@@ -14,5 +14,19 @@ export default Ember.Route.extend(AuthenticatedRouteMixin ,{
         //since these respond to dropdown configuration
 		laboratory: false
 		};
-    }
+    },
+    /**  
+  * handles case where someone tries to visit new contact route manually as a user not admin 
+  * in that case we transition back to search-contact
+  * @method setupController
+  */ 
+ setupController(controller, model) {
+    // Call _super for default behavior
+    this._super(controller, model);
+
+    if (this.get('session.data.authenticated.role')!=='Admin'){
+		  this.transitionTo('/search-contact');
+	}
+  }
+  
 });
