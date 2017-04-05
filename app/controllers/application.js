646cdf15b53a774982cec22e2ba50c42141ad933
Justin Barclay <justincbarclay@gmail.com>
2017-04-05 09:06:40 -0600

    Fix erroneous loading of wrong navbar, by adding role to session state and observing changes of that role in application view

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,5 +3,13 @@ import Ember from 'ember';
 //export default Ember.Controller.extend({
 export default Ember.Controller.extend({
      session: Ember.inject.service('session'),
-     isAdmin: (localStorage.getItem('role') === 'Admin')
+     isAdmin: "",
+     init(){
+         this._super(...arguments);
+         console.log(this.get('session.data.authenticated.role'));
+         this.addObserver('session.data.authenticated.role', function(){ 
+             this.set('isAdmin', this.get('session.data.authenticated.role')==='Admin');
+         });
+         this.set('isAdmin', this.get('session.data.authenticated.role')==='Admin');
+     }
 });
