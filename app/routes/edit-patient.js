40f9cc68d220356715955c9d9e3ea97fe2becbbc
erichsueh <erichsueh1337@hotmail.com>
2017-04-06 14:43:05 -0600

    forgot to add parsedate

--- app/routes/edit-patient.js
+++ app/routes/edit-patient.js
@@ -60,3 +60,18 @@ function fixNulls(data){
 
 	return fixed;
 }
+
+function parseDate(date){
+		if (date == ""){
+			return("");
+		}
+        var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+        var months = ["January","February","March","April","May","June","July", "August", "September", "October", "November", "December"];
+        var day = date.getDay() ;
+        var month = date.getMonth()  ;
+        var year = date.getFullYear();
+        var hours = date.getHours();
+        var mins = (date.getMinutes()<10?'0':'') + date.getMinutes();
+        var whole = days[day] +" "+ months[month] +" " + date.getDate().toString() +", "+ year.toString() + " "+ hours.toString() + ":" + mins.toString();
+        return(whole);
+}
